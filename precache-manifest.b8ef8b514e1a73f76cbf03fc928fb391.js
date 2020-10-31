8e9e2dd92f95488fe200ba8ea5af3297c15d14fd
ritwik jha <jha.ritwik.17.9@gmail.com>
2020-11-01 00:31:18 +0530

    Updates

---
+++ precache-manifest.b8ef8b514e1a73f76cbf03fc928fb391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b28d8f9204ef8280ce59db84471514",
+    "url": "/react-reviews/index.html"
+  },
+  {
+    "revision": "e999cc5a09c3fa84af98",
+    "url": "/react-reviews/static/css/main.e64ab053.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/react-reviews/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-reviews/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e999cc5a09c3fa84af98",
+    "url": "/react-reviews/static/js/main.2b754e9f.chunk.js"
+  },
+  {
+    "revision": "426fc16f2db2543aea9e",
+    "url": "/react-reviews/static/js/runtime-main.79a1a57c.js"
+  }
+]);
